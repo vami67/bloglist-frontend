7e534682c9a2ab4ec348a5e27513e01b31887671
Mika Vahlberg <mika.vahlberg@iki.fi>
2020-04-14 11:58:07 +0300

    5.3: blogilistan frontend, step3

--- src/App.js
+++ src/App.js
@@ -9,6 +9,10 @@ const App = () => {
   const [username, setUsername] = useState('')
   const [password, setPassword] = useState('')
 
+  const [title, setTitle] = useState('')
+  const [author, setAuthor] = useState('')
+  const [url, setUrl] = useState('')
+
   useEffect(() => {
     blogService.getAll().then(blogs =>
       setBlogs(blogs)
@@ -20,6 +24,7 @@ const App = () => {
     if (loggedUserJSON) {
       const user = JSON.parse(loggedUserJSON)
       setUser(user)
+      blogService.setToken(user.token)
     }
   }, [])
 
@@ -33,6 +38,7 @@ const App = () => {
         'loggedBlogappUser',
         JSON.stringify(user)
       )
+      blogService.setToken(user.token)
       setUser(user)
       setUsername('')
       setPassword('')
@@ -43,8 +49,53 @@ const App = () => {
 
   const handleLogout = () => {
     window.localStorage.removeItem('loggedBlogappUser')
-
+    blogService.setToken(null)
   }
+
+  const addBlog = (event) => {
+    event.preventDefault()
+    console.log(url)
+
+    blogService.createBlog({ title: title, author: author, url: url })
+      .then(returnedBlog => {
+        console.log(returnedBlog)
+        setBlogs(blogs.concat(returnedBlog))
+       
+        setUrl('')
+        setAuthor('')
+        setTitle('')
+      })
+  }
+
+  const blogForm = () => (
+    <form onSubmit={addBlog}>
+      <div>
+        title
+        <input
+          value={title}
+          name="title"
+          onChange={({ target }) => setTitle(target.value)}
+        />
+      </div>
+      <div>
+        author
+        <input
+          value={author}
+          name="author"
+          onChange={({ target }) => setAuthor(target.value)}
+        />
+      </div>
+      <div>
+        url
+        <input
+          value={url}
+          name="url"
+          onChange={({ target }) => setUrl(target.value)}
+        />
+      </div>
+      <button type="submit">save</button>
+    </form>
+  )
 
   if (user === null) {
     return (
@@ -68,8 +119,10 @@ const App = () => {
   return (
     <div>
       <h2>blogs</h2>
-
       <p>{user.name} logged in <button onClick={() => handleLogout()}>logout</button></p>
+
+      <h2>create new</h2>
+      {blogForm()}
 
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
--- src/services/blogs.js
+++ src/services/blogs.js
@@ -1,9 +1,22 @@
 import axios from 'axios'
 const baseUrl = '/api/blogs'
+let token = null
+
+const setToken = newToken => {
+  token = `bearer ${newToken}`
+}
 
 const getAll = () => {
   const request = axios.get(baseUrl)
   return request.then(response => response.data)
 }
 
-export default { getAll }
+const createBlog = async (blog) => {
+  const config = {
+    headers: { Authorization: token },
+  }
+  const response = await axios.post(baseUrl, blog, config)
+  return response.data
+}
+
+export default { getAll, createBlog, setToken }
