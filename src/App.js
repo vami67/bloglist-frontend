11a781cb475655bff02792730bb148adf51c1051
Mika Vahlberg <mika.vahlberg@iki.fi>
2020-04-14 10:44:23 +0300

    5.2: blogilistan frontend, step2

--- src/App.js
+++ src/App.js
@@ -15,18 +15,35 @@ const App = () => {
     )
   }, [])
 
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem('loggedBlogappUser')
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON)
+      setUser(user)
+    }
+  }, [])
+
   const handleLogin = async (event) => {
     event.preventDefault()
     try {
       const user = await loginService.login({
         username, password,
       })
+      window.localStorage.setItem(
+        'loggedBlogappUser',
+        JSON.stringify(user)
+      )
       setUser(user)
       setUsername('')
       setPassword('')
     } catch (exception) {
       console.log("Wrong credentials")
     }
+  }
+
+  const handleLogout = () => {
+    window.localStorage.removeItem('loggedBlogappUser')
+
   }
 
   if (user === null) {
@@ -52,7 +69,7 @@ const App = () => {
     <div>
       <h2>blogs</h2>
 
-      <p>{user.name} logged in</p>
+      <p>{user.name} logged in <button onClick={() => handleLogout()}>logout</button></p>
 
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
