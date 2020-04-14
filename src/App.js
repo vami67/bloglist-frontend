ff330fd513095b885d87f27e16ffe608e0c38602
Mika Vahlberg <mika.vahlberg@iki.fi>
2020-04-14 10:30:44 +0300

    5.1: blogilistan frontend, step1

--- src/App.js
+++ src/App.js
@@ -21,15 +21,11 @@ const App = () => {
       const user = await loginService.login({
         username, password,
       })
-
       setUser(user)
       setUsername('')
       setPassword('')
     } catch (exception) {
-      //setErrorMessage('wrong credentials')
-      setTimeout(() => {
-        //setErrorMessage(null)
-      }, 5000)
+      console.log("Wrong credentials")
     }
   }
 
@@ -55,9 +51,9 @@ const App = () => {
   return (
     <div>
       <h2>blogs</h2>
-      
+
       <p>{user.name} logged in</p>
-      
+
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
       )}
