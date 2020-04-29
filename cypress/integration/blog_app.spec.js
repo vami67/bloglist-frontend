4a49c556e764316c21c8688fa3fda7d96d532588
Mika Vahlberg <mika.vahlberg@iki.fi>
2020-04-29 09:10:25 +0300

    5.17: blogilistan end to end -testit, step1

--- cypress/integration/blog_app.spec.js
+++ cypress/integration/blog_app.spec.js
@@ -1,10 +1,28 @@
-describe('Blog app', function() {
-  beforeEach(function() {
+describe('Blog app', function () {
+  beforeEach(function () {
     cy.request('POST', 'http://localhost:3001/api/testing/reset')
+    // create here a user to backend
+    const user = {
+      name: 'Mika Vahlberg',
+      username: 'root',
+      password: 'String'
+    }
+    cy.request('POST', 'http://localhost:3001/api/users/', user)
     cy.visit('http://localhost:3000')
   })
 
-  it('Login form is shown', function() {
-    // ...
+  it('Login from is shown', function () {
+    cy.visit('http://localhost:3000')
+    cy.contains('login').click()
+  })
+
+  describe('Login', function () {
+    it('succeeds with correct credentials', function () {
+      // ...
+    })
+
+    it('fails with wrong credentials', function () {
+      // ...
+    })
   })
 })
